eed10befbd77f8a4b4328cd2c8cb905ed1f8da8a
Somekindofa <akbas.theo@protonmail.com>
2026-01-01 11:45:08 +0100

    Displaced the definiton of a function

--- lvl1/level1.py
+++ lvl1/level1.py
@@ -9,6 +9,12 @@ lvl1_file = "lvl1/level1_puzzle_input.txt"
 loader = InputLoader(lvl1_file)
 input = loader.content
 
+def input_to_signed_numbers(input: list[str]):
+        signs = [1 if re.search(r"R", i) else -1 for i in input]
+        numbers = [int(re.search(r"\d+", i).group()) for i in input]
+        signed_numbers = [s * n for s, n in zip(signs, numbers)]  
+        return signed_numbers
+
 if __name__ == "__main__":
     print("This is level 1")
     if not input:
@@ -22,13 +28,6 @@ if __name__ == "__main__":
     # for loop on the list of turns, retrieving the letter to convert to minus or plus
     # and retrieveing the number to accumulate on "acc"
     data_list = input.split()
-
-    def input_to_signed_numbers(input: list[str]):
-        signs = [1 if re.search(r"R", i) else -1 for i in input]
-        numbers = [int(re.search(r"\d+", i).group()) for i in input]
-        signed_numbers = [s * n for s, n in zip(signs, numbers)]  
-        return signed_numbers
-
     signedN = input_to_signed_numbers(data_list)
     for sn in signedN:
         acc += sn
